b197bea312da2e894940ef75702fd5b96a4927a9
Raymond <raymond631@qq.com>
2023-07-08 10:47:10 +0800

    配置跨域

--- yuetong-common/yuetong-common-gateway/src/main/java/cn/tdsmy/auth/SaTokenConfigure.java
+++ yuetong-common/yuetong-common-gateway/src/main/java/cn/tdsmy/auth/SaTokenConfigure.java
@@ -1,6 +1,8 @@
 package cn.tdsmy.auth;
 
+import cn.dev33.satoken.context.SaHolder;
 import cn.dev33.satoken.reactor.filter.SaReactorFilter;
+import cn.dev33.satoken.router.SaHttpMethod;
 import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaResult;
@@ -9,12 +11,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * [Sa-Token 权限认证] 配置类
+ * [Sa-Token 权限认证] 配置类（包含跨域处理）
  */
 @Slf4j
 @Configuration
 public class SaTokenConfigure {
-    // 注册 Sa-Token全局过滤器
+    /**
+     * 注册 Sa-Token全局过滤器
+     */
     @Bean
     public SaReactorFilter getSaReactorFilter() {
         return new SaReactorFilter()
@@ -42,6 +46,24 @@ public class SaTokenConfigure {
                 // 异常处理方法：每次setAuth函数出现异常时进入
                 .setError(e -> {
                     return SaResult.error(e.getMessage());
+                })
+                // 前置函数：在每次认证函数之前执行
+                .setBeforeAuth(obj -> {
+                    SaHolder.getResponse()
+                            // ---------- 设置跨域响应头 ----------
+                            // 允许指定域访问跨域资源
+                            .setHeader("Access-Control-Allow-Origin", "*")
+                            // 允许所有请求方式
+                            .setHeader("Access-Control-Allow-Methods", "*")
+                            // 允许的header参数
+                            .setHeader("Access-Control-Allow-Headers", "*")
+                            // 有效时间
+                            .setHeader("Access-Control-Max-Age", "3600")
+                    ;
+                    // 如果是预检请求，则立即返回到前端
+                    SaRouter.match(SaHttpMethod.OPTIONS)
+                            .free(r -> System.out.println("--------OPTIONS预检请求，不做处理"))
+                            .back();
                 })
                 ;
     }
