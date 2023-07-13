8d13082ed74bea97c8b38792c657ce60e86c24f6
DoubleYue <1139040315@qq.com>
2023-07-13 12:12:41 +0800

    初步合并

--- yuetong-ui/vite.config.ts
+++ yuetong-ui/vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
     https: false,
     proxy: {
       "/api": {
-        // target: "http://127.0.0.1:4523/m1/2962122-0-default",
-        target: "http://192.168.48.23:10000",
+        target: "http://127.0.0.1:4523/m1/2962122-0-default",
+        // target: "http://192.168.48.23:10000",
         // target: "http://localhost:10000//",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
