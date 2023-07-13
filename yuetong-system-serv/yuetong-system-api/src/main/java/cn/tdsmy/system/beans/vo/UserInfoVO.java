801f0f660c014196ed9a2716f8a0fa4cab3e23d1
Raymond <raymondli631@163.com>
2023-07-13 11:00:52 +0800

    bug fix

--- yuetong-system-serv/yuetong-system-api/src/main/java/cn/tdsmy/system/beans/vo/UserInfoVO.java
+++ yuetong-system-serv/yuetong-system-api/src/main/java/cn/tdsmy/system/beans/vo/UserInfoVO.java
@@ -1,5 +1,6 @@
 package cn.tdsmy.system.beans.vo;
 
+import cn.dev33.satoken.stp.SaTokenInfo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,6 @@ public class UserInfoVO {
     private String username;
     private int roleId;
     private String roleName;
+    private SaTokenInfo tokenInfo;
 }
 
