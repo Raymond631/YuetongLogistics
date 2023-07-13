b6a26b6e3447279ae37394c81c4af9d3327a87ef
Raymond <raymondli631@163.com>
2023-07-13 11:39:14 +0800

    bug fix

--- yuetong-system-serv/yuetong-system-impl/src/main/java/cn/tdsmy/system/YueTongSystemApplication.java
+++ yuetong-system-serv/yuetong-system-impl/src/main/java/cn/tdsmy/system/YueTongSystemApplication.java
@@ -2,6 +2,7 @@ package cn.tdsmy.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients(basePackages = {"cn.tdsmy"})
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan("cn.tdsmy")
 public class YueTongSystemApplication {
     public static void main(String[] args) {
