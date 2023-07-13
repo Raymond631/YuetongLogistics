6257db66385a175207211c3cff9ea02481967ca1
PRay <2519219483@qq.com>
2023-07-13 11:37:12 +0800

    承运单分页

--- yuetong-ui/src/views/carrier/bill/issue.vue
+++ yuetong-ui/src/views/carrier/bill/issue.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="container">
-    <navigationBar/>
+    <navigationBar />
     <div class="content">
-      <welcomeHeader/>
+      <welcomeHeader />
       <div class="issueBtn" @click="issueBtnClick">
         <label>开出承运单</label>
       </div>
       <label class="myCarriersLabel">我开出的承运单</label>
 
       <div class="main-form">
-        <div class="bill-block" v-for="(carrier,index) in myCarrierList">
+        <div class="bill-block" v-for="(carrier, index) in myCarrierList">
           <div class="driver">
             <label class="main-font">驾驶员</label>
             <div class="driver-info">
-              <el-button type="info" size="large" class="choDriBtn">未接收</el-button>
+              <el-button type="info" size="large" class="choDriBtn"
+                >未接收</el-button
+              >
             </div>
           </div>
           <div class="carrier-info">
@@ -25,20 +27,37 @@
             </div>
             <div class="time">
               <label class="third-font">配送时间</label>
-              <label v-text="'起: '+carrier.leaverDate" class="third-font"></label>
-              <label v-text="'终: '+carrier.receiveDate" class="third-font"></label>
+              <label
+                v-text="'起: ' + carrier.leaverDate"
+                class="third-font"
+              ></label>
+              <label
+                v-text="'终: ' + carrier.receiveDate"
+                class="third-font"
+              ></label>
             </div>
             <div class="receive-info">
               <label v-text="carrier.receiveCompany" class="title-font"></label>
               <label v-text="carrier.receiveAddress" class="third-font"></label>
-              <label v-text="carrier.receiveLinkman" class="second-font"></label>
+              <label
+                v-text="carrier.receiveLinkman"
+                class="second-font"
+              ></label>
               <label v-text="carrier.receivePhone" class="fourth-font"></label>
             </div>
           </div>
           <div class="manager-info">
-            <label class="main-font manager-id" v-text="'承运业务员编号: #'+carrier.fkUserID"></label>
+            <label
+              class="main-font manager-id"
+              v-text="'承运业务员编号: #' + carrier.fkUserID"
+            ></label>
             <div class="actions">
-              <el-button type="primary" class="infoBtn" @click="forDetails(index)">查看详情</el-button>
+              <el-button
+                type="primary"
+                class="infoBtn"
+                @click="forDetails(index)"
+                >查看详情</el-button
+              >
               <el-button type="danger" class="infoBtn">删除</el-button>
             </div>
           </div>
@@ -53,7 +72,9 @@
         <div class="form">
           <div class="form-title">
             <label>开出承运单</label>
-            <el-button class="closeBtn" type="danger" @click="closeForm">关闭</el-button>
+            <el-button class="closeBtn" type="danger" @click="closeForm"
+              >关闭</el-button
+            >
           </div>
           <div class="form-info">
             <label class="flex-item-title">发货信息</label>
@@ -114,18 +135,44 @@
               </div>
             </div>
             <label class="flex-item-title goods">货物信息</label>
-            <el-button type="primary" class="addGoodsBtn" @click="addGoodsBtnClick">+</el-button>
+            <el-button
+              type="primary"
+              class="addGoodsBtn"
+              @click="addGoodsBtnClick"
+              >+</el-button
+            >
             <div class="flex-item goods">
-              <div class="form-item" v-for="(goods,index) in goodsList" :key="index">
+              <div
+                class="form-item"
+                v-for="(goods, index) in goodsList"
+                :key="index"
+              >
                 <label>货物名</label>
-                <el-input type="text" v-model="goodsList[index].goodsName"></el-input>
+                <el-input
+                  type="text"
+                  v-model="goodsList[index].goodsName"
+                ></el-input>
                 <label>数量</label>
-                <el-input type="text" v-model="goodsList[index].amount"></el-input>
+                <el-input
+                  type="text"
+                  v-model="goodsList[index].amount"
+                ></el-input>
                 <label>重量</label>
-                <el-input type="text" v-model="goodsList[index].weight"></el-input>
+                <el-input
+                  type="text"
+                  v-model="goodsList[index].weight"
+                ></el-input>
                 <label>体积</label>
-                <el-input type="text" v-model="goodsList[index].volume"></el-input>
-                <el-button type="danger" class="deleteGoodsBtn" @click="deleteGoodsBtnClick(index)">删除</el-button>
+                <el-input
+                  type="text"
+                  v-model="goodsList[index].volume"
+                ></el-input>
+                <el-button
+                  type="danger"
+                  class="deleteGoodsBtn"
+                  @click="deleteGoodsBtnClick(index)"
+                  >删除</el-button
+                >
               </div>
             </div>
             <div class="actions">
@@ -133,103 +180,121 @@
               <el-button type="info">重置</el-button>
             </div>
           </div>
-
         </div>
-
       </div>
-      <div class="main-right">
-      </div>
+      <div class="main-right"></div>
     </div>
-
+    <div class="page">
+      <el-pagination
+        v-model:currentPage="paginationConfig.currentPage"
+        layout="total, prev, pager, next"
+        :page-size="paginationConfig.pageSize"
+        :total="paginationConfig.total"
+        @current-change="handlePageChange"
+      />
+    </div>
   </div>
-
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
 import navigationBar from "../../../components/navigationBar.vue";
 import welcomeHeader from "../../../components/header.vue";
-import {allMyCarrierList} from "@/api/carrier/bill/issue";
+import { allMyCarrierList } from "../../../api/carrier/bill/issue";
 
 export default defineComponent({
   name: "issue",
   data() {
     return {
-      myCarrierList: [{
-        "carriersId": 3,
-        "sendCompany": "qui",
-        "sendAddress": "云南省佳木斯市雁江区",
-        "sendLinkman": "elit ut sunt",
-        "sendPhone": "19840510269",
-        "receiveCompany": "ad Ut",
-        "receiveAddress": "四川省三沙市景谷傣族彝族自治县",
-        "receiveLinkman": "ut",
-        "receivePhone": "13138768380",
-        "leaverDate": "1988-01-06",
-        "receiveDate": null,
-        "finishedState": 1,
-        "insuranceCost": 9.0,
-        "transportCost": 81.0,
-        "otherCost": 23.0,
-        "totalCost": 113.0,
-        "remark": "labore tempor exercitation pariatur deserunt",
-        "account": "abc",
-        "checkInTime": "2023-07-12 06:11:28",
-        "alterTime": "2023-07-12 06:11:28",
-        "goodsList": null
-      }],
+      myCarrierList: [
+        {
+          carriersId: 3,
+          sendCompany: "qui",
+          sendAddress: "云南省佳木斯市雁江区",
+          sendLinkman: "elit ut sunt",
+          sendPhone: "19840510269",
+          receiveCompany: "ad Ut",
+          receiveAddress: "四川省三沙市景谷傣族彝族自治县",
+          receiveLinkman: "ut",
+          receivePhone: "13138768380",
+          leaverDate: "1988-01-06",
+          receiveDate: null,
+          finishedState: 1,
+          insuranceCost: 9.0,
+          transportCost: 81.0,
+          otherCost: 23.0,
+          totalCost: 113.0,
+          remark: "labore tempor exercitation pariatur deserunt",
+          account: "abc",
+          checkInTime: "2023-07-12 06:11:28",
+          alterTime: "2023-07-12 06:11:28",
+          goodsList: null,
+        },
+      ],
       issueCarrier: {
-        "sendCompany": "mollit amet minim",
-        "sendAddress": "山东省玉林市南区",
-        "sendLinkman": "non labore in",
-        "sendPhone": "19848185972",
-        "receiveCompany": "dolor exercitation commodo sit aute",
-        "receiveAddress": "海南省运城市铁东区",
-        "receiveLinkman": "irure aliquip ut enim",
-        "receivePhone": "13277832224",
-        "leaverDate": "2016-11-04",
-        "insuranceCost": 10,
-        "transportCost": 40,
-        "otherCost": 53,
-        "remark": "aliquip incididunt amet consequat nulla",
-        "goodsList": [
+        sendCompany: "mollit amet minim",
+        sendAddress: "山东省玉林市南区",
+        sendLinkman: "non labore in",
+        sendPhone: "19848185972",
+        receiveCompany: "dolor exercitation commodo sit aute",
+        receiveAddress: "海南省运城市铁东区",
+        receiveLinkman: "irure aliquip ut enim",
+        receivePhone: "13277832224",
+        leaverDate: "2016-11-04",
+        insuranceCost: 10,
+        transportCost: 40,
+        otherCost: 53,
+        remark: "aliquip incididunt amet consequat nulla",
+        goodsList: [
           {
-            "goodsName": "当叫八存并",
-            "amount": 38,
-            "weight": 24.12,
-            "volume": 11.34
+            goodsName: "当叫八存并",
+            amount: 38,
+            weight: 24.12,
+            volume: 11.34,
           },
           {
-            "goodsName": "者家头达",
-            "amount": 24,
-            "weight": 92.12,
-            "volume": 53.34
-          }
-        ]
+            goodsName: "者家头达",
+            amount: 24,
+            weight: 92.12,
+            volume: 53.34,
+          },
+        ],
       },
-      goodsList: [{
-        "goodsName": "",
-        "amount": 0,
-        "weight": 0,
-        "volume": 0
-      }],
+      paginationConfig: {
+        currentPage: 1, // 当前页码
+        pageSize: 4, // 每页显示的条数
+        pageCount: 1, //总共有多少页
+        total: 0, // 总条数
+      },
+      goodsList: [
+        {
+          goodsName: "",
+          amount: 0,
+          weight: 0,
+          volume: 0,
+        },
+      ],
       //分页参数
-      pageNum: 1,//当前页面number
-      pageSize: 5,//一页能显示的最多数据的数量
+      pageNum: 1, //当前页面number
+      pageSize: 5, //一页能显示的最多数据的数量
       showMask: false,
       showIssueCarrierForm: false,
     };
   },
-  components: {welcomeHeader, navigationBar},
+  components: { welcomeHeader, navigationBar },
   mounted() {
     this.ready();
   },
   methods: {
     ready() {
-      allMyCarrierList(1, 10).then((res: any) => {
+      allMyCarrierList(
+        this.paginationConfig.currentPage,
+        this.paginationConfig.pageSize
+      ).then((res: any) => {
         console.log(res.data.list);
         this.myCarrierList = res.data.list;
-      })
+        this.paginationConfig.total = res.data.total;
+      });
     },
     //点击开出承运单
     issueBtnClick() {
@@ -244,17 +309,20 @@ export default defineComponent({
     //添加货物
     addGoodsBtnClick() {
       this.goodsList.push({
-        "goodsName": "",
-        "amount": 0,
-        "weight": 0,
-        "volume": 0
+        goodsName: "",
+        amount: 0,
+        weight: 0,
+        volume: 0,
       });
     },
     //删除货物表单中的某一行
     deleteGoodsBtnClick(index: number) {
       this.goodsList.splice(index, 1);
     },
-    goBack() {
+    handlePageChange(val: number) {
+      this.paginationConfig.currentPage = val;
+      console.log("当前页面数为：" + val);
+      this.ready();
     },
   },
 });
--- yuetong-ui/src/views/carrier/bill/reception.vue
+++ yuetong-ui/src/views/carrier/bill/reception.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="container">
-    <navigationBar/>
+    <navigationBar />
     <div class="content">
-      <welcomeHeader/>
+      <welcomeHeader />
       <div class="title">
         <div class="search">
-          <el-input type="text" class="search-input" v-model="search" placeholder="请搜索用户"></el-input>
+          <el-input
+            type="text"
+            class="search-input"
+            v-model="search"
+            placeholder="请搜索用户"
+          ></el-input>
         </div>
         <div class="actions">
           <div class="action1">
@@ -14,40 +19,91 @@
         </div>
       </div>
       <div class="main-form">
-        <div class="bill-block" v-for="(reception,index) in receptionList">
-          <label class="carrierId" v-text="reception.carrier.carriersId"></label>
+        <div class="bill-block" v-for="(reception, index) in receptionList">
+          <label
+            class="carrierId"
+            v-text="reception.carrier.carriersId"
+          ></label>
           <div class="driver">
-            <el-avatar type="image" class="image"
-                       src="https://img02.sogoucdn.com/app/a/100520093/8379901cc65ba509-45c21ceb904429fc-e8e0ced72c7814e527ca276e0fe48673.jpg"></el-avatar>
+            <el-avatar
+              type="image"
+              class="image"
+              src="https://img02.sogoucdn.com/app/a/100520093/8379901cc65ba509-45c21ceb904429fc-e8e0ced72c7814e527ca276e0fe48673.jpg"
+            ></el-avatar>
             <div class="driver-info">
-              <el-tag class="main-font driver-tag"
-                      v-text="'驾驶员#'+reception.driver.driverId +' '+reception.driver.name"></el-tag>
+              <el-tag
+                class="main-font driver-tag"
+                v-text="
+                  '驾驶员#' +
+                  reception.driver.driverId +
+                  ' ' +
+                  reception.driver.name
+                "
+              ></el-tag>
               <label class="third-font" v-text="reception.driver.phone"></label>
             </div>
           </div>
           <div class="carrier-info">
             <div class="send-info">
-              <label v-text="reception.carrier.sendCompany" class="title-font"></label>
-              <label v-text="reception.carrier.sendAddress" class="third-font"></label>
-              <label v-text="reception.carrier.sendLinkman" class="second-font"></label>
-              <label v-text="reception.carrier.sendPhone" class="fourth-font"></label>
+              <label
+                v-text="reception.carrier.sendCompany"
+                class="title-font"
+              ></label>
+              <label
+                v-text="reception.carrier.sendAddress"
+                class="third-font"
+              ></label>
+              <label
+                v-text="reception.carrier.sendLinkman"
+                class="second-font"
+              ></label>
+              <label
+                v-text="reception.carrier.sendPhone"
+                class="fourth-font"
+              ></label>
             </div>
             <div class="time">
               <label class="third-font">配送时间</label>
-              <label v-text="'起: '+reception.carrier.leaverDate" class="third-font"></label>
-              <label v-text="'终: '+reception.carrier.receiveDate" class="third-font"></label>
+              <label
+                v-text="'起: ' + reception.carrier.leaverDate"
+                class="third-font"
+              ></label>
+              <label
+                v-text="'终: ' + reception.carrier.receiveDate"
+                class="third-font"
+              ></label>
             </div>
             <div class="receive-info">
-              <label v-text="reception.carrier.receiveCompany" class="title-font"></label>
-              <label v-text="reception.carrier.receiveAddress" class="third-font"></label>
-              <label v-text="reception.carrier.receiveLinkman" class="second-font"></label>
-              <label v-text="reception.carrier.receivePhone" class="fourth-font"></label>
+              <label
+                v-text="reception.carrier.receiveCompany"
+                class="title-font"
+              ></label>
+              <label
+                v-text="reception.carrier.receiveAddress"
+                class="third-font"
+              ></label>
+              <label
+                v-text="reception.carrier.receiveLinkman"
+                class="second-font"
+              ></label>
+              <label
+                v-text="reception.carrier.receivePhone"
+                class="fourth-font"
+              ></label>
             </div>
           </div>
           <div class="manager-info">
-            <label class="main-font manager-id" v-text="'处理管理员: #'+reception.carrier.account"></label>
+            <label
+              class="main-font manager-id"
+              v-text="'处理管理员: #' + reception.carrier.account"
+            ></label>
             <div class="actions">
-              <el-button type="primary" class="infoBtn" @click="receptCarrierBtnClick(index)">接收</el-button>
+              <el-button
+                type="primary"
+                class="infoBtn"
+                @click="receptCarrierBtnClick(index)"
+                >接收</el-button
+              >
               <el-button type="danger" class="infoBtn">删除</el-button>
             </div>
           </div>
@@ -61,16 +117,30 @@
       <div class="receptDialog" v-if="showReceptDialog">
         <div class="form-title">
           <label>接收承运单</label>
-          <el-button class="closeBtn" type="danger" @click="closeForm">关闭</el-button>
+          <el-button class="closeBtn" type="danger" @click="closeForm"
+            >关闭</el-button
+          >
         </div>
         <div class="form">
           <el-descriptions title="承运单信息" size="large" column="3" border>
-            <el-descriptions-item label="承运单编号">{{ receptCarrier.carriersId }}</el-descriptions-item>
-            <el-descriptions-item label="登记时间">{{ receptCarrier.checkInTime }}</el-descriptions-item>
-            <el-descriptions-item label="发货公司">{{ receptCarrier.sendCompany }}</el-descriptions-item>
-            <el-descriptions-item label="发货地址">{{ receptCarrier.sendAddress }}</el-descriptions-item>
-            <el-descriptions-item label="收货公司">{{ receptCarrier.receiveCompany }}</el-descriptions-item>
-            <el-descriptions-item label="收货地址">{{ receptCarrier.receiveAddress }}</el-descriptions-item>
+            <el-descriptions-item label="承运单编号">{{
+              receptCarrier.carriersId
+            }}</el-descriptions-item>
+            <el-descriptions-item label="登记时间">{{
+              receptCarrier.checkInTime
+            }}</el-descriptions-item>
+            <el-descriptions-item label="发货公司">{{
+              receptCarrier.sendCompany
+            }}</el-descriptions-item>
+            <el-descriptions-item label="发货地址">{{
+              receptCarrier.sendAddress
+            }}</el-descriptions-item>
+            <el-descriptions-item label="收货公司">{{
+              receptCarrier.receiveCompany
+            }}</el-descriptions-item>
+            <el-descriptions-item label="收货地址">{{
+              receptCarrier.receiveAddress
+            }}</el-descriptions-item>
           </el-descriptions>
         </div>
         <div class="actions">
@@ -78,75 +148,95 @@
           <el-button type="info" size="large">取消</el-button>
         </div>
       </div>
-
+    </div>
+    <div class="page">
+      <el-pagination
+        v-model:currentPage="paginationConfig.currentPage"
+        layout="total, prev, pager, next"
+        :page-size="paginationConfig.pageSize"
+        :total="paginationConfig.total"
+        @current-change="handlePageChange"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
 import navigationBar from "../../../components/navigationBar.vue";
 import welcomeHeader from "../../../components/header.vue";
 import ytTable from "../../../components/yt-table.vue";
-import {allReceptionCarriers} from "@/api/carrier/bill/reception";
+import { allReceptionCarriers } from "../../../api/carrier/bill/reception";
 
 export default defineComponent({
   name: "reception",
   data() {
     return {
-      search: '',
-      "receptionList": [{
-        "carrier": {
-          "carriersId": 0,
-          "sendCompany": "",
-          "sendAddress": "",
-          "sendLinkman": "",
-          "sendPhone": "",
-          "receiveCompany": "",
-          "receiveAddress": null,
-          "receiveLinkman": "",
-          "receivePhone": "",
-          "leaverDate": "",
-          "receiveDate": "",
-          "finishedState": 0,
-          "insuranceCost": 0,
-          "transportCost": 0,
-          "otherCost": 0,
-          "totalCost": 0,
-          "remark": "",
-          "account": 0,
-          "checkInTime": "",
-          "alterTime": ""
+      search: "",
+      paginationConfig: {
+        currentPage: 1, // 当前页码
+        pageSize: 15, // 每页显示的条数
+        pageCount: 1, //总共有多少页
+        total: 0, // 总条数
+      },
+      receptionList: [
+        {
+          carrier: {
+            carriersId: 0,
+            sendCompany: "",
+            sendAddress: "",
+            sendLinkman: "",
+            sendPhone: "",
+            receiveCompany: "",
+            receiveAddress: null,
+            receiveLinkman: "",
+            receivePhone: "",
+            leaverDate: "",
+            receiveDate: "",
+            finishedState: 0,
+            insuranceCost: 0,
+            transportCost: 0,
+            otherCost: 0,
+            totalCost: 0,
+            remark: "",
+            account: 0,
+            checkInTime: "",
+            alterTime: "",
+          },
+          driver: {
+            driverId: 1,
+            name: "张三",
+            sex: "男",
+            birth: "1988-10-01",
+            phone: "12345678900",
+            idCard: "123456789123456789",
+            fkTeamId: 1,
+            state: "Working",
+            remark: "labore tempor exercitation pariatur deserunt",
+            checkInTime: "2023-07-12 14:53:23",
+            alterTime: "2023-07-12 14:53:23",
+          },
         },
-        "driver": {
-          "driverId": 1,
-          "name": "张三",
-          "sex": "男",
-          "birth": "1988-10-01",
-          "phone": "12345678900",
-          "idCard": "123456789123456789",
-          "fkTeamId": 1,
-          "state": "Working",
-          "remark": "labore tempor exercitation pariatur deserunt",
-          "checkInTime": "2023-07-12 14:53:23",
-          "alterTime": "2023-07-12 14:53:23"
-        }
-      }],
+      ],
       showMask: false,
       showReceptDialog: false,
       receptCarrier: {},
     };
   },
-  components: {welcomeHeader, navigationBar, ytTable},
+  components: { welcomeHeader, navigationBar, ytTable },
   mounted() {
     this.ready();
   },
   methods: {
     ready() {
-      allReceptionCarriers(1, 10).then((res: any) => {
+      allReceptionCarriers(
+        this.paginationConfig.currentPage,
+        this.paginationConfig.pageSize
+      ).then((res: any) => {
         this.receptionList = res.data.list;
-        console.log(this.receptionList)
-      })
+        console.log(this.receptionList);
+        this.paginationConfig.total = res.data.total;
+      });
     },
     //点击接受按钮
     receptCarrierBtnClick(index: number) {
@@ -159,7 +249,10 @@ export default defineComponent({
       this.showMask = false;
       this.showReceptDialog = false;
     },
-    goBack() {
+    handlePageChange(val: number) {
+      this.paginationConfig.currentPage = val;
+      console.log("当前页面数为：" + val);
+      this.ready();
     },
   },
 });
